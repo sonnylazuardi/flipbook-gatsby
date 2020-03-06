47a244c2473e8bd3d4b4eaf9d4545055a1cdecea
Sonny Lazuardi Hermawan <sonny.lazuardi@grabtaxi.com>
2020-03-06 23:31:38 +0800

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter SaaS Marketing`,
-    description: `A simple one page marketing starter for saas companies.`,
-    author: `Keegan Burkett`,
+    title: `Figma Flopbook`,
+    description: `Animate your figma design`,
+    author: `Sonny Lazuardi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
